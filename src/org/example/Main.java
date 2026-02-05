f3e3c854525f8e8013b73996a17b3b0342b88aba
mk-404lab <alsgur9984@naver.com>
2026-02-06 00:43:05 +0900

    feat: 1단계 구현

---
+++ src/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    static void main() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명령) ");
+
+        String cmd = input.nextLine();
+    }
+}
